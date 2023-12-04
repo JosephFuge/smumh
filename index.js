b8057ef7cd36a9afd153559cf7b263e898f06cb9
Cole1213 <150396075+Cole1213@users.noreply.github.com>
2023-12-04 13:49:47 -0700

    Update index.js
    added knex

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
-
 const PORT_NUM = 3000;
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "localhost",
+    user: "postgres",
+    password: "postgres",
+    database: "intex",
+    port: 5432
+  }
+});
 
 // Frontend static middleware
 app.use(express.static('public'));
